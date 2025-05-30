682b08cac3e28f453d5e66fa964c78715f9363d1
EC2 Default User <ec2-user@ip-172-31-0-114.ap-northeast-1.compute.internal>
2025-05-30 13:44:29 +0000

    各投稿の投稿日時(created_at)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @books = @user.books
+    @books = @user.books.order(created_at: :desc)
     @book = Book.new
   end
 
